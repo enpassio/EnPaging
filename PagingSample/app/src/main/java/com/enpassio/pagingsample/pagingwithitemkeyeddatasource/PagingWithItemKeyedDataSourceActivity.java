4c7764509f93c5807676565cd75ed9519bd92b2f
enpassio <abhishekraj@abhishekraj.me>
2018-11-23 01:03:26 +0530

    Moved back from androidX to support v7 library

--- PagingSample/app/src/main/java/com/enpassio/pagingsample/pagingwithitemkeyeddatasource/PagingWithItemKeyedDataSourceActivity.java
+++ PagingSample/app/src/main/java/com/enpassio/pagingsample/pagingwithitemkeyeddatasource/PagingWithItemKeyedDataSourceActivity.java
@@ -1,10 +1,9 @@
 package com.enpassio.pagingsample.pagingwithitemkeyeddatasource;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.enpassio.pagingsample.R;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class PagingWithItemKeyedDataSourceActivity extends AppCompatActivity {
 
--- PagingSample/app/src/main/java/com/enpassio/pagingsample/pagingwithpagekeyeddatasource/PagingWithPageKeyedDataSourceActivity.java
+++ PagingSample/app/src/main/java/com/enpassio/pagingsample/pagingwithpagekeyeddatasource/PagingWithPageKeyedDataSourceActivity.java
@@ -1,10 +1,9 @@
 package com.enpassio.pagingsample.pagingwithpagekeyeddatasource;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.enpassio.pagingsample.R;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class PagingWithPageKeyedDataSourceActivity extends AppCompatActivity {
 
